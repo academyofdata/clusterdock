ecb7d5e8a29fbf195c477bb06b9c31a39c16085f
fixone <fixone@users.noreply.github.com>
2017-06-11 15:36:17 +0300

    Create getindocker.sh

---
+++ getindocker.sh
@@ -0,0 +1,1 @@
+sudo docker exec -ti `sudo docker ps | grep clusterdock | grep secondary | head -1 | awk '{print $1}'` bash -c "wget -qO- https://raw.githubusercontent.com/academyofdata/clusterdock/master/get2hdfs.sh | bash -s"

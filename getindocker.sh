d02d39c2bc8f216863e94b06dbc653a494fd6fa2
fixone <fixone@users.noreply.github.com>
2017-06-11 15:49:46 +0300

    Create getindocker.sh

--- getindocker.sh
+++ getindocker.sh
@@ -1,1 +1,1 @@
-sudo docker exec -i `sudo docker ps | grep clusterdock | grep secondary | head -1 | awk '{print $1}'` bash -c "wget -qO- https://raw.githubusercontent.com/academyofdata/clusterdock/master/get2hdfs.sh | bash -s"
+sudo docker exec -i `sudo docker ps | grep clusterdock | grep secondary | head -1 | awk '{print $1}'` bash -c "echo "nameserver 8.8.8.8" >> /etc/resolv.conf;wget -qO- https://raw.githubusercontent.com/academyofdata/clusterdock/master/get2hdfs.sh | bash -s"

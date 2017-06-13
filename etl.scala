a66d6d9cb5560d95697148346bb160cdc767d4b6
fixone <fixone@users.noreply.github.com>
2017-06-13 19:05:46 +0300

    Create etl.scala

--- etl.scala
+++ etl.scala
@@ -1,3 +1,5 @@
+import com.databricks.spark.avro._
+
 val ratings = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("/input/ratings.csv")
 val movies = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("/input/movies.csv")
 val users = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("/input/users.csv")
@@ -5,6 +7,9 @@ val users = sqlContext.read.format("com.databricks.spark.csv").option("header", 
 ratings.saveAsParquetFile("/data/1m/ratings.parquet")
 movies.saveAsParquetFile("/data/1m/movies.parquet")
 users.saveAsParquetFile("/data/1m/users.parquet")
+
+sqlContext.setConf("spark.sql.avro.compression.codec", "deflate")
+sqlContext.setConf("spark.sql.avro.deflate.level", "5")
 
 ratings.write.format("com.databricks.spark.avro").save("/data/1m/ratings.avro")
 movies.write.format("com.databricks.spark.avro").save("/data/1m/movies.avro")

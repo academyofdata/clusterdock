31da86ddc4dd2a8153bb1c580f66fcf49c4327d7
fixone <fixone@users.noreply.github.com>
2017-06-13 16:58:19 +0300

    Create getlensdata.sh

--- getlensdata.sh
+++ getlensdata.sh
@@ -4,4 +4,6 @@ yum install -y unzip
 cd /tmp
 wget http://files.grouplens.org/datasets/movielens/ml-20m.zip
 unzip ml-20m.zip
-#HADOOP_USER_NAME=hive hdfs dfs -put /tmp/ml-20m
+#if everything went fine we should have all the movielens data files in /tmp/ml-20m, so put them in HDFS
+HADOOP_USER_NAME=hive hdfs dfs -mkdir /input/ml20m
+HADOOP_USER_NAME=hive hdfs dfs -put /tmp/ml-20m/* /input/ml20m

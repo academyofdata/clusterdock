5a8a81159eb3847a4d93f14fca28608a2a2a62fa
fixone <fixone@users.noreply.github.com>
2017-06-13 15:47:07 +0300

    Create getlensdata.sh

---
+++ getlensdata.sh
@@ -0,0 +1,5 @@
+#this should run in one of the clusterdock containers!
+#first get the file from the grouplens.org site
+yum install -y unzip
+cd /tmp
+wget http://files.grouplens.org/datasets/movielens/ml-20m.zip

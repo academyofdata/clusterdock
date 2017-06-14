b830d0c7b1eb0dd555744fad5af580a7be82b311
fixone <fixone@users.noreply.github.com>
2017-06-14 21:12:17 +0300

    Create etl.scala

--- etl.scala
+++ etl.scala
@@ -1,6 +1,6 @@
 import com.databricks.spark.avro._
 
-
+//valid values for compression codec: snappy, deflate, uncompressed
 sqlContext.setConf("spark.sql.avro.compression.codec", "snappy")
 sqlContext.setConf("spark.sql.avro.deflate.level", "5")
 sqlContext.setConf("spark.sql.parquet.compression.codec", "snappy")
@@ -9,13 +9,17 @@ val ratings = sqlContext.read.format("com.databricks.spark.csv").option("header"
 val movies = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("/input/movies.csv")
 val users = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("/input/users.csv")
 
+
 ratings.saveAsParquetFile("/data/1m/ratings.parquet")
 movies.saveAsParquetFile("/data/1m/movies.parquet")
 users.saveAsParquetFile("/data/1m/users.parquet")
-//valid values for compression codec: snappy, deflate, uncompressed
 
 
 ratings.write.format("com.databricks.spark.avro").save("/data/1m/ratings.avro")
 movies.write.format("com.databricks.spark.avro").save("/data/1m/movies.avro")
 users.write.format("com.databricks.spark.avro").save("/data/1m/users.avro")
+
+//if we want that parquet/avro files are saved in a single partition we do it like this
+//ratings.coalesce(1).saveAsParquetFile("/data/1m/ratings.parquet")
+//ratings.coalesce(1).write.format("com.databricks.spark.avro").save("/data/1m/ratings.avro")
 

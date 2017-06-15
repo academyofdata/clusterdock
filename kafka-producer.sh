85a6fac231e534984b64d004ccd135c13c94a540
fixone <fixone@users.noreply.github.com>
2017-06-15 16:50:04 +0300

    Update kafka-producer.sh

--- kafka-producer.sh
+++ kafka-producer.sh
@@ -1,4 +1,1 @@
-cd /tmp
-wget -q https://raw.githubusercontent.com/academyofdata/clusterdock/master/random-string-for-kafka-producer.sh
-chmod +x ./random-string-for-kafka-producer.sh
-./random-string-for-kafka-producer.sh > kafka-console-producer --broker-list node-2.cluster:9092 --topic aod
+wget -qO- https://raw.githubusercontent.com/academyofdata/clusterdock/master/random-string-for-kafka-producer.sh | bash | kafka-console-producer --broker-list node-2.cluster:9092 --topic aod

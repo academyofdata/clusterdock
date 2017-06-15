3081c644cb705fa14ac05be01b02ef51961b316e
fixone <fixone@users.noreply.github.com>
2017-06-15 15:57:49 +0300

    Create kafka-streaming.py
    borrowed from https://www.cloudera.com/documentation/enterprise/5-5-x/topics/spark_streaming.html

---
+++ kafka-streaming.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+
+import sys
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: kafka-streaming.py <zk> <topic>", file=sys.stderr)
+        exit(-1)
+
+    sc = SparkContext(appName="KafkaStreamingWC")
+    ssc = StreamingContext(sc, 10)
+
+    zkQuorum, topic = sys.argv[1:]
+    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+    lines = kvs.map(lambda x: x[1])
+    counts = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
+    counts.pprint()
+
+    ssc.start()
+    ssc.awaitTermination()

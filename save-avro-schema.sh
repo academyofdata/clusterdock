0c78d7ee1b22d4a3d8c287eba455ef5addd9db9d
fixone <fixone@users.noreply.github.com>
2017-06-17 20:37:47 +0300

    Update save-avro-schema.sh

--- save-avro-schema.sh
+++ save-avro-schema.sh
@@ -1,3 +1,6 @@
 #run in container
-
-wget -O- https://raw.githubusercontent.com/academyofdata/clusterdock/master/get-avro-schema.sh | bash -s "/data/1m/ratings.avro"
+for file in ratings.avro ratings.parquet 
+do
+echo "doing $file"
+#wget -O- https://raw.githubusercontent.com/academyofdata/clusterdock/master/get-avro-schema.sh | bash -s "/data/1m/ratings.avro"
+done

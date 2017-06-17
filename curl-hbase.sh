3128287f8720006bee091a2760655c1dcfcd3846
fixone <fixone@users.noreply.github.com>
2017-06-17 14:26:09 +0300

    Update curl-hbase.sh

--- curl-hbase.sh
+++ curl-hbase.sh
@@ -2,7 +2,7 @@ function encode {
   echo $1 | tr -d "\n" | base64
 }
 
-
+curl -vi -X GET -H "Accept: application/json" 'http://localhost:20550/test/schema'
 #commands that create an HBase table, using HBase REST server (needs to be enabled/installed if not there)
 #first create a table called 'test' with a CF called 'data'
 curl -v -X PUT 'http://localhost:20550/test/schema' -H "Accept: application/json" -H "Content-Type: application/json" -d '{"@name":"test","ColumnSchema":[{"name":"data"}]}'

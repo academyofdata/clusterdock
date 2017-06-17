59106251b2eb3797f6f5cdcd062a32432bb544fb
fixone <fixone@users.noreply.github.com>
2017-06-17 16:53:34 +0300

    Update get-avro-schema.sh

--- get-avro-schema.sh
+++ get-avro-schema.sh
@@ -1,4 +1,4 @@
-#scripts that tries to get the schema from an Avro file
+#script that tries to get the schema from an Avro file
 #to be run in one of the containers
 #uses an utility avro-tools.jar that ships with the Avro distribution
 #in CDH it is wrapped in a script /usr/bin/avro-tools

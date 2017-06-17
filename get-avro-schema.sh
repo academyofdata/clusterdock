2245239a7210ff28dc989f1189ebf887179f2bb0
fixone <fixone@users.noreply.github.com>
2017-06-17 16:57:55 +0300

    Update get-avro-schema.sh

--- get-avro-schema.sh
+++ get-avro-schema.sh
@@ -2,4 +2,7 @@
 #to be run in one of the containers
 #uses an utility avro-tools.jar that ships with the Avro distribution
 #in CDH it is wrapped in a script /usr/bin/avro-tools
-hdfs dfs -ls -d $1/*.avro | head -1 | awk '{print $8}'
+cd /tmp
+FILE=`hdfs dfs -ls -d $1/*.avro | head -1 | awk '{print $8}'`
+hdfs dfs -cat $FILE | head -bytes 10K > sample.avro
+avro-tools getschema ./sample.avro > schema.avsc

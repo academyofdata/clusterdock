229784c629356c983a459f1fca462105c64e031c
fixone <fixone@users.noreply.github.com>
2017-06-17 20:42:13 +0300

    Update save-avro-schema.sh

--- save-avro-schema.sh
+++ save-avro-schema.sh
@@ -2,5 +2,5 @@
 for file in ratings.avro ratings-all.avro users.avro movies.avro
 do
   wget -O- https://raw.githubusercontent.com/academyofdata/clusterdock/master/get-avro-schema.sh | bash -s "/data/1m/$file"
-  HADOOP_USER_NAME=hdfs hdfs dfs -put /tmp/schema.avsc /metadata/$file
+  HADOOP_USER_NAME=hdfs hdfs dfs -put /tmp/schema.avsc /metadata/$file.schema
 done

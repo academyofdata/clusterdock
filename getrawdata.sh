405c491a9e966439710401f42d1c0261aaac2ea6
fixone <fixone@users.noreply.github.com>
2017-06-18 18:11:44 +0300

    Update getrawdata.sh

--- getrawdata.sh
+++ getrawdata.sh
@@ -1,7 +1,7 @@
 wget https://raw.githubusercontent.com/academyofdata/inputs/master/movies.csv
 wget https://raw.githubusercontent.com/academyofdata/inputs/master/users.csv
 wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings2.csv
-wget https://raw.githubusercontent.com/academyofdata/inputs/blob/master/ratings.csv.gz 
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings.csv.gz
 gunzip ratings.csv.gz 
 mv ratings.csv ratings-all.csv
 mv ratings2.csv ratings.csv

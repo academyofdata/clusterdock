7a04dd6aad6cd0f65063d83598de8703c17c5033
fixone <fixone@users.noreply.github.com>
2017-09-04 13:22:58 +0300

    Create gen-avro-parquet.sh

---
+++ gen-avro-parquet.sh
@@ -0,0 +1,6 @@
+#this script transforms the HDFS stored CSV files to AVRO and Parquet formats using spark-shell and a small scala script
+cd /tmp
+#gets the ETL script into /tmp, the saved file will be called etl.scala
+wget -q https://raw.githubusercontent.com/academyofdata/clusterdock/master/avro-parquet.scala
+#runs spark-shell providing the script as input
+HADOOP_USER_NAME=spark spark-shell --packages com.databricks:spark-csv_2.10:1.5.0 com.databricks:spark-avro_2.10:3.2.0 -i avro-parquet.scala 

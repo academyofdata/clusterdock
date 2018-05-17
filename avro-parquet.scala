387c37d30e3f053d2d8ceeb6c08bb62e7b1bbfc0
fixone <fixone@users.noreply.github.com>
2018-05-17 22:10:16 +0300

    Update avro-parquet.scala

--- avro-parquet.scala
+++ avro-parquet.scala
@@ -5,9 +5,9 @@ sqlContext.setConf("spark.sql.avro.compression.codec", "snappy")
 sqlContext.setConf("spark.sql.avro.deflate.level", "5")
 sqlContext.setConf("spark.sql.parquet.compression.codec", "snappy")
 
-val ratings_all = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("/input/ratings-all/ratings-all.csv")
+val ratings_all = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("/data/ratings-all/ratings-all.csv")
 
 //if we want that parquet/avro files are saved in a single partition we do it like this
-ratings_all.coalesce(1).saveAsParquetFile("/input/ratings_all_parquet")
-ratings_all.coalesce(1).write.format("com.databricks.spark.avro").save("/input/ratings_all_avro")
+ratings_all.coalesce(1).saveAsParquetFile("/data/ratings_all_parquet")
+ratings_all.coalesce(1).write.format("com.databricks.spark.avro").save("/data/ratings_all_avro")
 
